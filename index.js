6627be4803ac6c3eeaa57098b992f903edda4538
tienhung164 <>
2021-08-22 01:51:45 +0700

    ok2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app= express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = 3001 || process.env.PORT;
+const port = (3001 || process.env.PORT);
 const delay= require('delay')
 
 
